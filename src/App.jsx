ac93f99ad6108d3510dde7b6a0d208a57641d2f2
Gandals-hash <phiritshepiso99@gmail.com>
2023-10-31 12:19:20 +0200

    feat(styling): improve UI mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import PrimaryButton from './components/PrimaryButton.jsx';
 function App() {
 
   return (
-    <div className="p-4 w-full h-full md:m-12 flex md:flex-row  justify-center text-primary-color">
-      <div className="w-full justify-center md:mr-16 md:max-w-lg flex flex-col">
+    <div className="min-h-screen flex-col-reverse flex md:flex-row justify-center text-primary-color">
+      <div className="p-4 w-full md:ite justify-center 2xl:mr-16 md:m-12 md:max-w-lg flex flex-col">
         <h1 className="font-bold text-5xl md:text-6xl mb-8">Stay updated!</h1>
         <span className="md:text-xl mb-8"> Join 60,000+ product managers receiving monthly updates on:</span>
         <ItemList />
@@ -15,7 +15,8 @@ function App() {
         </div>
       </div>
       <div className=''>
-        <img src="/images/illustration-sign-up-desktop.svg" alt="Illustration Sign Up" />
+        <img className='hidden md:block w-full h-full' src="/images/illustration-sign-up-desktop.svg" alt="Desktop Illustration Sign Up" />
+        <img className='md:hidden flex mb-12 w-full' src="/images/illustration-sign-up-mobile.svg" alt="Mobile Illustration Sign Up " />
       </div>
     </div>
   );
